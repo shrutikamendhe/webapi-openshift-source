2ea93e733242e8988f1b1251c7173f67949096cf
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-07-22 13:12:09 +0530

    sample web api methods added

---
+++ src/webapi_openshift_source/Controllers/ToDoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi_openshift_source.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        /// <summary>
+        /// Static todo list. Common for all
+        /// </summary>
+        internal static List<Module.ToDo> _todo = new List<Module.ToDo>();
+
+
+        // GET: api/todo
+        [HttpGet]
+        public List<Module.ToDo> Get()
+        {
+            return _todo;
+        }
+
+        // GET api/todo/5
+        [HttpGet("{index}")]
+        public Module.ToDo Get(int index)
+        {
+            try { return _todo[index]; } catch (Exception) { return null; }
+        }
+
+        // POST api/post
+        [HttpPost]
+        public void Post([FromBody]string title, [FromBody]string detail)
+        {
+            _todo.Add(new Module.ToDo() {
+                Title = title,
+                Detail= title
+            });
+        }
+
+        // PUT api/put/5
+        [HttpPut("{index}")]
+        public void Put(int index, [FromBody]string title, [FromBody]string detail, [FromBody]string state)
+        {
+            _todo[index].Title = title;
+            _todo[index].Detail = detail;
+            if (state.ToLower() == "done")
+                _todo[index].State = Module.ToDo.STATE.DONE;
+        }
+
+        // DELETE api/delete/5
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            _todo.RemoveAt(index);
+        }
+    }
+}
---
+++ src/webapi_openshift_source/Module/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_openshift_source.Module
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+
+        public string Detail { get; set; }
+
+        public STATE State { get; set; }
+
+        public enum STATE
+        {
+            NOTSTARTED,
+            INPROGRESS,
+            DONE,
+            REMOVED
+        }
+    }
+}
