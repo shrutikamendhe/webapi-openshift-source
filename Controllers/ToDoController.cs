a04a8767b5883822543104d856390bb216d2aaaf
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-07-24 11:07:01 +0530

    removed methods

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -3,58 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
-// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using System.Web.Http;
 
 namespace webapi_openshift_source.Controllers
 {
 
+    // <copyright file="ToDoController.cs" company="Click2Cloud">
+    // Copyright (c) 2015 All Rights Reserved
+    // </copyright>
+    // <author>Shrutika Mendhe</author>
+    // <date>07/24/2015 10:42 AM </date>
+    // <summary>The controller for ToDo</summary>
     [Route("api/[controller]")]
     public class ToDoController : Controller
     {
         /// <summary>
-        /// Static todo list. Common for all
+        /// The static object of ToDo List. This object is common for all request
         /// </summary>
         internal static List<ToDo> _todo = new List<ToDo>();
 
-
-
-        // GET: api/todo
+        /// <summary>
+        /// Get all items in todo list object.
+        /// GET: api/todo
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<ToDo> Get()
         {
             return _todo;
         }
 
-        // GET api/todo/5
-        [HttpGet("{index}")]
-        public ToDo Get(int index)
-        {
-            try { return _todo[index]; } catch (Exception) { return null; }
-        }
-
-        // POST api/post
+        /// <summary>
+        /// Add item in todo list object.
+        /// POST api/todo
+        /// </summary>
+        /// <param name="toDo">Send item in query string of request</param>
         [HttpPost]
-        public void Post([FromBody]string title, [FromBody]string detail)
+        public void Post([FromUri]ToDo toDo)
         {
-            _todo.Add(new ToDo()
-            {
-                Title = title,
-                Detail = title
-            });
+            _todo.Add(toDo);
         }
 
-        // PUT api/put/5
+        /// <summary>
+        /// Update state of an item in todo list object.
+        /// PUT api/put/5
+        /// </summary>
+        /// <param name="index">Index of an item</param>
+        /// <param name="state">state either ACTIVE or DONE</param>
         [HttpPut("{index}")]
-        public void Put(int index, [FromBody]string title, [FromBody]string detail, [FromBody]string state)
+        public void Put(int index, [FromUri]string state)
         {
-            _todo[index].Title = title;
-            _todo[index].Detail = detail;
-            if (state.ToLower() == "done")
-                _todo[index].State = state;
+            _todo[index].State = state.ToUpper();
         }
 
-        // DELETE api/delete/5
+        /// <summary>
+        /// Delete perticualr item from todo list object
+        /// DELETE api/delete/5
+        /// </summary>
+        /// <param name="index">Index of an item</param>
         [HttpDelete("{index}")]
         public void Delete(int index)
         {
